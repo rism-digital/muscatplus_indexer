11c12acbba2062f151dbc7eca3c77f9b9c0b0b7c
Andrew Hankinson <andrew.hankinson@gmail.com>
2020-11-25 00:06:14 +0100

    Add a final commit statement once all indexing is done.

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ from indexer.index_people import index_people
 from indexer.index_places import index_places
 from indexer.index_sources import index_sources
 from indexer.index_subjects import index_subjects
+from indexer.helpers.solr import solr_conn
 
 log_config: Dict = yaml.full_load(open('logging.yml', 'r'))
 idx_config: Dict = yaml.full_load(open('index_config.yml', 'r'))
@@ -24,6 +25,8 @@ def main() -> bool:
     ins: bool = index_institutions(idx_config)
     hld: bool = index_holdings(idx_config)
     sub: bool = index_subjects(idx_config)
+
+    solr_conn.commit()
 
     return src and ppl and plc and ins and hld and sub
 

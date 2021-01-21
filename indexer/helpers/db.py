116abb481830bbb50ab672ae8ae2772a5fe4d41b
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-01-21 16:33:49 +0100

    Fixed: Bug with host connection

--- indexer/helpers/db.py
+++ indexer/helpers/db.py
@@ -10,13 +10,13 @@ idx_config: Dict = yaml.full_load(open('index_config.yml', 'r'))
 config: Dict = {
     "user": idx_config['mysql']['username'],
     "password": idx_config['mysql']['password'],
-    "db": idx_config['mysql']['database']
+    "db": idx_config['mysql']['database'],
+    "host": idx_config['mysql']['server'],
 }
 
 
 mysql_connection = MySQLdb.connect(
     **config,
-    host=idx_config['mysql']['server'],
     cursorclass=SSDictCursor
 )
 

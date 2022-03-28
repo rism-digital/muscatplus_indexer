fc9b2b0737ef18e8fa11ba3bda97a60996743676
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-03-28 14:43:38 +0200

    New: Index 580 (now in) relationships

--- indexer/index_institutions.py
+++ indexer/index_institutions.py
@@ -31,7 +31,12 @@ def _get_institution_groups(cfg: dict) -> Generator[tuple, None, None]:
                         FROM {dbname}.holdings_to_institutions AS hi
                         LEFT JOIN {dbname}.holdings AS hh ON hi.holding_id = hh.id
                         WHERE hi.institution_id = i.id)
-                        AS holdings_count
+                        AS holdings_count,
+                    (SELECT GROUP_CONCAT(DISTINCT CONCAT_WS('|', reli.id, reli.siglum, reli.name) SEPARATOR '\n')
+                        FROM {dbname}.institutions_to_institutions AS rela
+                        LEFT JOIN {dbname}.institutions AS reli ON  reli.id = rela.institution_b_id
+                        WHERE rela.institution_a_id = i.id)
+                        AS related_institutions
                     FROM {dbname}.institutions AS i
                     WHERE i.siglum IS NOT NULL OR
                         ((SELECT COUNT(hi.holding_id) FROM {dbname}.holdings_to_institutions AS hi WHERE hi.institution_id = i.id) > 0 OR

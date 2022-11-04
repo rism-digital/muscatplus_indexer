1bb2939a198420214ab5d26f12645fb207e44a44
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-11-04 12:58:19 +0100

    WIP: Digital Object URLs
    Refs rism-digital/rism-online-issues#245

--- indexer/records/digital_object.py
+++ indexer/records/digital_object.py
@@ -1,5 +1,7 @@
+import logging
 import textwrap
 
+log = logging.getLogger("muscat_indexer")
 BASE_MUSCAT_URL: str = "https://muscat.rism.info/system/digital_objects/attachments/"
 
 
@@ -9,15 +11,26 @@ def create_digital_object_index_document(record: dict, cfg: dict) -> dict:
     linked_record_type: str = record["object_link_type"]
     linked_record_id: int = record["object_link_id"]
     filename: str = record["attachment_file_name"]
+    media_type: str = record['attachment_content_type']
 
+    # NB: URL format example 000/000/014/original/240.jpg
+    # Pad the id to 9 chars, split it into groups of 3, and insert "/" between them.
     left_padded_id: str = f"{record_id:09}"
     left_padded_path: str = "/".join(textwrap.wrap(left_padded_id, 3))
 
-    # 000/000/014/original/240.jpg
-
-    original_url: str = f"{BASE_MUSCAT_URL}{left_padded_path}/original/{filename}"
-    thumb_url: str = f"{BASE_MUSCAT_URL}{left_padded_path}/thumb/{filename}"
-    medium_url: str = f"{BASE_MUSCAT_URL}{left_padded_path}/medium/{filename}"
+    urls: dict = {}
+    if media_type in ("image/png", "image/jpeg"):
+        urls = {
+            "original_url_s": f"{BASE_MUSCAT_URL}{left_padded_path}/original/{filename}",
+            "thumb_url_s": f"{BASE_MUSCAT_URL}{left_padded_path}/thumb/{filename}",
+            "medium_url_s": f"{BASE_MUSCAT_URL}{left_padded_path}/medium/{filename}"
+        }
+    elif media_type == "application/xml":
+        urls = {
+            "encoding_url_s": f"{BASE_MUSCAT_URL}{left_padded_path}/incipits/{filename}"
+        }
+    else:
+        log.warning("Could not determine a media URL for type %s on object %s", media_type, record_id)
 
     digital_object: dict = {
         "type": "dobject",
@@ -26,9 +39,7 @@ def create_digital_object_index_document(record: dict, cfg: dict) -> dict:
         "linked_type_s": linked_record_type.lower(),
         "description_s": record.get("description"),
         "media_type_s": record.get("attachment_content_type"),
-        "original_url_s": original_url,
-        "thumb_url_s": thumb_url,
-        "medium_url_s": medium_url
+        **urls
     }
 
     return digital_object

5cb48f7ae3d693418477412705acf2bb9adaad7c
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-03-22 14:50:08 +0100

    Further marc parsing refactors

--- indexer/helpers/marc.py
+++ indexer/helpers/marc.py
@@ -1,47 +1,24 @@
-import logging
-from typing import Optional, TypedDict, Iterator
+from typing import Optional
 
 import pymarc
 
-MarcSubField = list[str]
 
-log = logging.getLogger("muscat_indexer")
-
-
-class MarcField(TypedDict):
-    tag: str
-    indicators: Optional[list]
-    subfields: Optional[list[pymarc.CodedSubfield]]
-    data: Optional[str]
-
-
-def _parse_field(line: str) -> MarcField:
+def _parse_field(line: str) -> pymarc.Field:
     # General format: =TAG  ##$afoo$bbar
     tag_value: str = line[1:4]
 
     # Control fields are those in the <010 range. They do not have
     # subfields, but have the data encoded in them directly.
     control: bool = tag_value.isdigit() and int(tag_value) < 10
-    subfields: Optional[list[pymarc.CodedSubfield]]
-    indicators: Optional[list[str]]
     if control:
-        subfields = None
-        indicators = None
-        data = line[6:]
+        return pymarc.Field(tag=tag_value, data=line[6:])
     else:
         ind_value: str = line[6:8]
         indicators: list = list(ind_value)
         sub_value: str = line[9:]
         subf_list: list = sub_value.split("$") if sub_value else []
         subfields: list[pymarc.CodedSubfield] = [_parse_subf(itm) for itm in subf_list if itm != '']
-        data = None
-
-    return {
-        "tag": tag_value,
-        "indicators": indicators,
-        "subfields": subfields,
-        "data": data
-    }
+        return pymarc.Field(tag=tag_value, indicators=indicators, subfields=subfields)
 
 
 def _parse_subf(subf_value: str) -> pymarc.CodedSubfield:
@@ -62,13 +39,11 @@ def create_marc(record: str) -> pymarc.Record:
     :return: an instance of a pymarc.Record
     """
     lines: list = record.split("\n")
-    fields: Iterator[MarcField] = (_parse_field(line) for line in lines if line and line != '')
-    r: pymarc.Record = pymarc.Record()
+    fields: list[pymarc.Field] = [_parse_field(line) for line in lines if line and line != '']
+    p_record: pymarc.Record = pymarc.Record()
+    p_record.add_field(*fields)
 
-    all_fields = (pymarc.Field(**field) for field in fields)
-    r.add_field(*all_fields)
-
-    return r
+    return p_record
 
 
 def create_marc_list(marc_records: Optional[str]) -> list[pymarc.Record]:

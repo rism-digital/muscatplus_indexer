2cd2613a256a4e40d9e9f68751c4666766cc8a2a
Andrew Hankinson <andrew.hankinson@gmail.com>
2025-06-04 15:11:40 +0200

    Fixed: create a knowable ID for tombstone records

--- indexer/records/tombstone.py
+++ indexer/records/tombstone.py
@@ -2,10 +2,14 @@ from typing import Any
 
 
 def create_tombstone_index_document(record, cfg: dict) -> dict[str, Any]:
+    record_type_id: str = record["item_type"].lower()
+    record_id: str = record["item_id"]
+
     tombstone_object: dict = {
+        "id": f"tombstone_{record_type_id}_{record_id}",
         "type": "tombstone",
-        "record_type_s": record["item_type"].lower(),
-        "record_id": record["item_id"],
+        "record_type_s": record_type_id,
+        "record_id": record_id,
         "display_name_s": record["name"],
         "removed_dt": record["deleted"].strftime("%Y-%m-%dT%H:%M:%SZ"),
     }

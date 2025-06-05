e3f9c521f255e6286f35b4a93daf1bf80edc75db
Andrew Hankinson <andrew.hankinson@gmail.com>
2025-06-05 15:05:08 +0200

    Updates for work catalogues

---
+++ indexer/processors/publication.py
@@ -0,0 +1,104 @@
+import pymarc
+
+from indexer.helpers.datelib import process_date_statements
+from indexer.helpers.utilities import (
+    external_resource_data,
+    get_related_institutions,
+    get_related_people,
+    normalize_id,
+    to_solr_multi,
+)
+
+
+def _get_creator_name(record: pymarc.Record) -> str | None:
+    if "100" not in record:
+        return None
+
+    creator: pymarc.Field = record["100"]
+    name: str = creator["a"].strip()
+    dates: str = f" ({d})" if (d := creator.get("d")) else ""
+
+    return f"{name}{dates}"
+
+
+def _get_creator_data(record: pymarc.Record) -> list | None:
+    if "100" not in record:
+        return None
+
+    record_id: str = normalize_id(record["001"].value())
+    publication_id: str = f"publication_{record_id}"
+    creator = get_related_people(record, publication_id, "publication", fields=("100",))
+    if not creator:
+        return None
+
+    creator[0]["relationship"] = "cre"
+    return creator
+
+
+def _get_earliest_latest_dates(record: pymarc.Record) -> list[int] | None:
+    date_statements: list | None = to_solr_multi(record, "260", "c")
+    if not date_statements:
+        return None
+
+    record_id: str = normalize_id(record["001"].value())
+
+    return process_date_statements(date_statements, record_id)
+
+
+def _get_earliest_latest_dates_dtr(previously_computed: list[int] | None) -> str | None:
+    # Takes the output of the _get_earliest_latest_dates function and creates a Solr DateRange Statement.
+    if not previously_computed:
+        return None
+
+    first = previously_computed[0]
+    last = previously_computed[1]
+
+    return f"[{first} TO {last}]"
+
+
+def _get_related_people_data(record: pymarc.Record) -> list | None:
+    if "700" not in record:
+        return None
+
+    publication_id: str = f"publication_{record['001'].value()}"
+    people = get_related_people(
+        record, publication_id, "publication", fields=("700",), ungrouped=True
+    )
+
+    return people or None
+
+def _get_related_institutions_data(record: pymarc.Record) -> list | None:
+    if "710" not in record:
+        return None
+    publication_id: str = f"publication_{record['001'].value()}"
+    institutions = get_related_institutions(
+        record, publication_id, "publication", fields=("710",)
+    )
+
+    return institutions or None
+
+
+def _get_external_resources_data(record: pymarc.Record) -> list | None:
+    """
+    Fetch the external links defined on the record. Note that this will *not* index the links that are linked to
+    material group descriptions -- those are handled in the material group indexing section above.
+    :param record: A pymarc record
+    :return: A list of external links. This will be serialized to a string for storage in Solr.
+    """
+    if "856" not in record:
+        return None
+
+    resources: list = [
+        external_resource_data(f) for f in record.get_fields("856") if f
+    ]
+
+    return resources if resources else None
+
+
+def _get_iiif_manifest_uris(record: pymarc.Record) -> list | None:
+    if "856" not in record:
+        return None
+
+    fields: list[pymarc.Field] = record.get_fields("856")
+    return [f["u"] for f in fields if "x" in f and "IIIF" in f["x"]]
+
--- indexer/records/work.py
+++ indexer/records/work.py
@@ -8,25 +8,25 @@ from indexer.helpers.utilities import (
     get_bibliographic_references_json,
     normalize_id,
 )
+from indexer.processors import publication as publication_processor
 from indexer.processors import work as work_processor
-from indexer.processors import work_catalogue as work_catalogue_processor
 from indexer.records.incipits import get_work_incipits
 
 work_profile: dict = yaml.full_load(open("profiles/works.yml"))  # noqa: SIM115
-work_catalogues_profile: dict = yaml.full_load(open("profiles/work_catalogues.yml"))  # noqa: SIM115
+publications_profile: dict = yaml.full_load(open("profiles/publications.yml"))  # noqa: SIM115
 
 
 def create_work_catalogue_index_document(record: dict, cfg: dict) -> dict:
     catalogue: str = record["marc_source"]
     marc_record: pymarc.Record = create_marc(catalogue)
     rism_id: str = normalize_id(marc_record["001"].value())
-    work_catalogue_id: str = f"publication_{rism_id}"
+    publication_id: str = f"publication_{rism_id}"
 
     work_ids_raw: list = w.split("\n") if (w := record["work_ids"]) else []
     work_ids: list = [f"work_{wid}" for wid in work_ids_raw]
 
     catalogue_core: dict = {
-        "id": work_catalogue_id,
+        "id": publication_id,
         "type": "publication",
         "rism_id": rism_id,
         "full_rism_id": f"publications/{rism_id}",
@@ -36,7 +36,7 @@ def create_work_catalogue_index_document(record: dict, cfg: dict) -> dict:
     }
 
     additional_fields: dict = process_marc_profile(
-        work_catalogues_profile, work_catalogue_id, marc_record, work_catalogue_processor
+        publications_profile, publication_id, marc_record, publication_processor
     )
 
     catalogue_core.update(additional_fields)
